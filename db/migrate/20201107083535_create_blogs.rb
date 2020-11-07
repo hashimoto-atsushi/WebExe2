ddfcb874690027f2e64d4e5cff20e2c995d7f4ef
atsushi hashimoto <newhattcy@joy.ocn.ne.jp>
2020-11-07 18:19:40 +0900

    graphql、graphiql-railsをインストール

--- db/migrate/20201107083535_create_blogs.rb
+++ db/migrate/20201107083535_create_blogs.rb
@@ -1,8 +1,8 @@
 class CreateBlogs < ActiveRecord::Migration[5.2]
   def change
     create_table :blogs do |t|
-      t.spring :title
-      t.text :content
+      t.string :title
+      t.string :text
 
       t.timestamps
     end
